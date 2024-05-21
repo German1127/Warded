acaf3fda8837102e925bf85ff0567bdb6a9f8bbb
German1127 <kleverk2023.com>
2024-05-21 18:56:13 -0300

    add animation

--- lib/BotonesConIconos/BotonAlarma.dart
+++ lib/BotonesConIconos/BotonAlarma.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
 
-class BotonAlarma extends StatelessWidget {
+class BotonAlarma extends StatefulWidget {
+  @override
+  _BotonAlarmaDestello createState() => _BotonAlarmaDestello();
+}
+
+class _BotonAlarmaDestello extends State<BotonAlarma> with SingleTickerProviderStateMixin {
+  bool _isRed = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 1), _changeColor);
+  }
+
+  void _changeColor() {
+    setState(() {
+      _isRed = !_isRed;
+    });
+    Future.delayed(Duration(seconds: 1), _changeColor);
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         print('¡Aca pongan lo que quieran que haga el boton SIN EL PRINT!');
       },
-      child: Container(
-        width: 70.0,
-        height: 70.0,
-        padding: const EdgeInsets.all(2.0),
-        decoration: const BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(10.0),
-            topRight: Radius.circular(10.0),
-            bottomLeft: Radius.circular(10.0),
-            bottomRight: Radius.circular(10.0),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 1000),
+        width: 50.0,
+        height: 50.0,
+        padding: const EdgeInsets.all(0.5),
+        decoration: BoxDecoration(
+          color: _isRed ? Colors.red : Colors.black,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(50.0),
+            topRight: Radius.circular(50.0),
+            bottomLeft: Radius.circular(50.0),
+            bottomRight: Radius.circular(50.0),
           ),
         ),
         child: Image.asset(
