fe15524e6f5a5d4f3753b487eb54f79f3f166f65
German1127 <kleverk2023.com>
2024-05-31 21:53:49 -0300

    funcionalidad del scaner ya pronto, con los permisos de usar la camara en el archivo .xml

--- lib/botones/boton_unirse_grupo.dart
+++ lib/botones/boton_unirse_grupo.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:barcode_scan2/barcode_scan2.dart';
 
 class BotonUnirseGrupo extends StatelessWidget {
   final String text;
 
   const BotonUnirseGrupo({Key? key, required this.text}) : super(key: key);
 
+  Future<void> scanQR() async {
+    try {
+      ScanResult qrResult = await BarcodeScanner.scan();
+      print(qrResult.rawContent);
+    } catch (ex) {
+      print(ex);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () {
-        // Aca va la funcion que le des al tocar el boton
+        scanQR();
       },
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
